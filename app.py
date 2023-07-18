42ee579a1008386bd5e1777d520090e72d1648be
Gabor Szabo <gabor@szabgab.com>
2023-07-18 11:16:54 +0300

    update project

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
-from flask import Flask
+import os
+
+from flask import Flask, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+def get_mongodb():
+    mongodb_host = os.environ.get("MONGODB_HOST")
+    if not mongodb_host:
+        exit("Missing MONGODB_HOST environment")
+    dbname = os.environ.get("MONGODB_DATABASE")
+    if not dbname:
+        exit("Missing MONGODB_DATABASE")
+
+    client = MongoClient("mongodb://{mongodb_host}", connectTimeoutMS=5000, socketTimeoutMS=5000)
+    return client[ dbname ]
 
 @app.route("/")
 def main():
     return "main page"
 
 
+@app.route("/mongodb", methods=['POST'])
+def mongodb_post():
+    text = request.form.get('text', '')
+    db = get_mongodb()
+    return text
+    result = db.storage.insert_one({"text": text})
+    return "done"
+
+@app.route("/mongodb", methods=['GET'])
+def mongodb_get():
+    db = get_mongodb()
+    result = db.packages.find()
+    return "qqrq"
--- test_app.py
+++ test_app.py
@@ -7,3 +7,15 @@ def test_main():
     assert rv.status == '200 OK'
     assert rv.data.decode('utf-8') == 'main page'
 
+def test_mongodb():
+    web = app.app.test_client()
+
+    rv = web.post('/mongodb', data={ "text": "foo bar" })
+    #assert rv.status == '200 OK'
+    #assert rv.data.decode('utf-8') == 'done'
+
+    rv = web.get('/mongodb')
+    assert rv.status == '200 OK'
+    print(rv.data)
+    #assert rv.data.decode('utf-8') == 'done'
+
